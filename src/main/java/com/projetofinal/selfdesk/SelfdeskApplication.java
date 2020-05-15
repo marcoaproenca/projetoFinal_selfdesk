b79b2eac9af51078c4dc105c1b5d3269d85c3ffd
markofurlan <marko.furlan@hotmail.com>
2020-05-15 10:37:10 -0300

    Primeiro Commit

---
+++ src/main/java/com/projetofinal/selfdesk/SelfdeskApplication.java
@@ -0,0 +1,13 @@
+package com.projetofinal.selfdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetofinal/selfdesk/SelfdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetofinal.selfdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelfdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
